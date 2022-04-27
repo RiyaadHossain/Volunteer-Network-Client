95f9b36b41ad2de9bf7a30fc919e8873867f9e2a
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-27 15:14:06 +0600

    Navbar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../../Logos/Group 1329.png";
+import './Navbar.css'
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <nav className="container mx-auto flex justify-between items-center h-20">
+      <div className="logo">
+        <img className="w-32" src={Logo} alt="" />
+      </div>
+      <div className="nav-link">
+        <NavLink className={({isActive}) => isActive ? 'active-link' : 'link'} to="/">Home</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'active-link' : 'link'} to="/donation">Donation</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'active-link' : 'link'} to="/events">Events</NavLink>
+        <NavLink className={({isActive}) => isActive ? 'active-link' : 'link'} to="/blog">Blog</NavLink>
+        <button className="nav-btn">Register</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
