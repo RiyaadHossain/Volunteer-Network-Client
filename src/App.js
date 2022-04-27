2bd8d743ad7e474c4773b80af695097deb63033c
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-27 15:03:10 +0600

    components and react tools added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import LogIn from './Components/Auth/LogIn/LogIn';
+import SignUp from './Components/Auth/SignUp/SignUp';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Registered from './Components/Registered/Registered';
+import VolunteerList from './Components/VolunteerList/VolunteerList';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={ <Home/>}/>
+        <Route path='/login' element={<LogIn/> }/>
+        <Route path='/signup' element={<SignUp/> }/>
+        <Route path='/volunteerlist' element={<VolunteerList/> }/>
+        <Route path='/registered' element={<Registered/> }/>
+      </Routes>
     </div>
   );
 }
